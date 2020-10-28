c5aa344157a0b65f40c23372a244d8bca1e31634
Infarh <shmachilin@yandex.ru>
2020-10-28 13:12:45 +0300

    Чтение файла данных в список значений координат излучателей

--- LinearArrayBeamCalculation/Program.cs
+++ LinearArrayBeamCalculation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace LinearArrayBeamCalculation
@@ -16,6 +17,22 @@ namespace LinearArrayBeamCalculation
                 return;
             }
 
+            //StreamReader reader = File.OpenText(file_name);
+            StreamReader reader = new StreamReader(file_name);
+
+            //string line = reader.ReadLine();
+            //double[] X = new double[10];
+            List<double> X_list = new List<double>();
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+
+                double x = double.Parse(line);
+                X_list.Add(x);
+                Console.WriteLine("x = {0:f3}", x);
+            }
+
+            reader.Close();
 
         }
     }
