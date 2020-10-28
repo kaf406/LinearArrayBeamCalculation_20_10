c688cdf19a64470884404722bad747e1150c0e46
Infarh <shmachilin@yandex.ru>
2020-10-28 13:06:13 +0300

    Добавьте файлы проекта.

---
+++ LinearArrayBeamCalculation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace LinearArrayBeamCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string file_name = "LinearArray.csv";
+
+            if (!File.Exists(file_name))
+            {
+                Console.WriteLine("Файл данных не существует!");
+                Console.ReadLine();
+                return;
+            }
+
+
+        }
+    }
+}
